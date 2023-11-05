66b5827f27ba59ffd6baf54a0f91b49fdec34a24
WaysoonProgramms <krokodilovanton@gmail.com>
2023-11-05 15:39:54 +0300

    Initial commit

---
+++ app/src/main/java/com/waysoonprogramms/globalreading/MainActivity.java
@@ -0,0 +1,14 @@
+package com.waysoonprogramms.globalreading;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
